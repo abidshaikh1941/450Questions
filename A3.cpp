959db29c0541b7707c3046a5f004c0b7d1e0f77b
Abid Shaikh <69729821+abidshaikh1941@users.noreply.github.com>
2021-01-05 20:18:48 +0530

    Create A3.cpp

---
+++ A3.cpp
@@ -0,0 +1,82 @@
+Kth smallest element 
+Medium Accuracy: 46.66% Submissions: 15594 Points: 4
+Given an array arr[] and a number K where K is smaller than size of array, the task is to find the Kth smallest element in the given array. It is given that all array elements are distinct.
+
+Expected Time Complexity: O(n)
+/*uses quick sort which is n to n^2 ranged*/
+Input:
+The first line of input contains an integer T, denoting the number of testcases. Then T test cases follow. Each test case consists of three lines. First line of each testcase contains an integer N denoting size of the array. Second line contains N space separated integer denoting elements of the array. Third line of the test case contains an integer K.
+
+Output:
+For each testcase, in a newline, print the kth smallest element.
+
+Yout Task:
+Complete kthSmallest and return the kth smallest element.
+
+Constraints:
+1 <= T <= 100
+1 <= N <= 105
+1 <= arr[i] <= 105
+1 <= K <= N
+
+Example:
+Input:
+2
+6
+7 10 4 3 20 15
+3
+5
+7 10 4 20 15
+4
+
+Output:
+7
+15
+
+Explanation:
+Testcase 1: 3rd smallest element in the given array is 7.
+Testcase 2: 4th smallest element in the given array is 15
+
+// { Driver Code Starts
+//Initial function template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int kthSmallest(int *, int, int, int);
+ 
+int main()
+{
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+    
+    int test_case;
+    cin>>test_case;
+    while(test_case--)
+    {
+        int number_of_elements;
+        cin>>number_of_elements;
+        int a[number_of_elements];
+        
+        for(int i=0;i<number_of_elements;i++)
+            cin>>a[i];
+            
+        int k;
+        cin>>k;
+        cout<<kthSmallest(a, 0, number_of_elements-1, k)<<endl;
+    }
+    return 0;
+}// } Driver Code Ends
+
+
+//User function template for C++
+
+// arr : given array
+// l : starting index of the array i.e 0
+// r : ending index of the array i.e size-1
+// k : find kth smallest element and return using this function
+int kthSmallest(int arr[], int l, int r, int k) {
+    //code here
+    sort(arr,arr+r+1);
+    return arr[k-1];
+}
